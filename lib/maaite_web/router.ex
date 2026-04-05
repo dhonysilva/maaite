060d616fbcbffd13c16a84d744f78ea607f9832a
Dhony Silva <dhony.dev@gmail.com>
2026-04-05 10:47:09 -0300

    Metadada dashboard

--- lib/maaite_web/router.ex
+++ lib/maaite_web/router.ex
@@ -17,7 +17,7 @@ defmodule MaaiteWeb.Router do
   scope "/", MaaiteWeb do
     pipe_through :browser
 
-    get "/", PageController, :home
+    live "/", CatalogLive
   end
 
   # Other scopes may use custom stacks.
